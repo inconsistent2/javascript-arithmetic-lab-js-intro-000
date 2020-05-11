badc74a1551435324e7b1f6071bfbb18ae6923e1
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-11 12:20:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function add(x,y){
-  return x+y
+function add(a,b){
+  return a+b
+}
 
+function subtract(a,b){
+  return a-b
+}
+
+function multiply (a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
 }
